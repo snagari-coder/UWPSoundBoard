f11c1ca0ee289b7019d749a8bcdcf8ec9a10c4a9
snagari-coder <62784275+snagari-coder@users.noreply.github.com>
2020-05-06 20:38:01 -0700

    Initial commit

---
+++ Model/Sound.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPSoundBoard.Model
+{
+    public enum SoundCategory
+    {
+        Animals,
+        Cartoons,
+        Taunts,
+        Warnings
+
+    }
+    public class Sound
+    {
+        public string Name { get; set; }
+        public SoundCategory Category { get; set; }
+        public string AudioFileLocation { get; set; }
+
+        public string ImageFileLocation { get; set; }
+
+        public Sound(string name, SoundCategory category)
+        {
+            Name = name;
+            Category = category;
+            AudioFileLocation = $"/Assets/Audio/{category}/{name}.wav";
+            ImageFileLocation = $"/Assets/Images/{category}/{name}.png";
+        }
+    }
+}
